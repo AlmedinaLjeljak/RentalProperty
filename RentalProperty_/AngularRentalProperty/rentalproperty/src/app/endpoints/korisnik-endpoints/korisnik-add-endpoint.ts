e9e3e18cdbdf4576c1b543295d86e7e78d081cf2
AlmedinaLjeljak <almedina.ljeljak@edu.fit.ba>
2025-03-02 00:11:18 +0100

    Dodavanje endpointa

---
+++ RentalProperty_/AngularRentalProperty/rentalproperty/src/app/endpoints/korisnik-endpoints/korisnik-add-endpoint.ts
@@ -0,0 +1,27 @@
+import {Injectable} from "@angular/core";
+import {MyBaseEndpoint} from "../MyBaseEndpoint";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Config} from "../../config";
+
+@Injectable({providedIn: 'root'})
+export class KorisnikAddEndpoint implements  MyBaseEndpoint<KorisnikAddRequest, void>{
+  constructor(public httpClient:HttpClient) { }
+  Handle(request: KorisnikAddRequest): Observable<void> {
+    let url=Config.adresa + `Korisnik-Add`;
+
+    return this.httpClient.post<void>(url, request);
+  }
+}
+export interface KorisnikAddRequest {
+  ime: string
+  prezime: string
+  username: string
+  password: string
+  slika: string | undefined
+  brojTelefona: string
+  gradID: number
+  spolID: number
+  drzavaID: number
+}
+
