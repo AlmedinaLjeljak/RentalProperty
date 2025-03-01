bcc944daa756620f233ca5caf2f74e53992d1192
AlmedinaLjeljak <almedina.ljeljak@edu.fit.ba>
2025-03-02 00:23:17 +0100

    Endpoints

---
+++ RentalProperty_/AngularRentalProperty/rentalproperty/src/app/endpoints/recenzije-endpoints/recenzije-add-endpoint.ts
@@ -0,0 +1,22 @@
+import {Injectable} from "@angular/core";
+import {MyBaseEndpoint} from "../MyBaseEndpoint";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Config} from "../../config";
+
+@Injectable({providedIn: 'root'})
+export class RecenzijaAddEndpoint implements  MyBaseEndpoint<RecenzijaAddRequest, void>{
+  constructor(public httpClient:HttpClient) { }
+  Handle(request: RecenzijaAddRequest): Observable<void> {
+    let url=Config.adresa + `Recenzija-Add`;
+
+    return this.httpClient.post<void>(url, request);
+  }
+}
+export interface RecenzijaAddRequest {
+  ime: string
+  prezime: string
+  tekst: string
+  slika: string | undefined
+}
+
